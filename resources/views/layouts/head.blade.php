b7f6529e4982d6eb445eb39db346d44e95e89fdb
ffenggg <wwfeng1999@gmail.com>
2026-08-19 16:25:28 +0800

    a

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,7 +3,7 @@
     name="viewport"
     content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-<title>Lao Yang Kopitiam</title>
+<title>WildFire</title>
 
 <meta name="description" content="" />
 
