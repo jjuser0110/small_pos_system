06610fd65822c4867e65688b614f995967ed6c9c
ffenggg <wwfeng1999@gmail.com>
2026-09-17 18:47:31 +0800

    a

--- resources/views/counter.blade.php
+++ resources/views/counter.blade.php
@@ -594,10 +594,11 @@ async function loadTables() {
 }
 
 const FLOOR_LAYOUT = [
-    [8, 9, 10],
-    [5, 6, 7],
-    [null, 3, 4],
-    [null, 1, 2],
+    [13, 14, 15],
+    [10, 11, 12],
+    [7, 8, 9],
+    [4, 5, 6],
+    [1, 2, 3],
 ];
 
 function chunkBy(arr, sizes) {
